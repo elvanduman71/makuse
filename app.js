1f821f0a2337c15270b23e8ef262b01c5efbacd2
elvanduman71 <elvanduman71@gmail.com>
2022-07-13 20:59:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const socket = require ('socket.io');
+
+const app=express();
+
+
+
+app.use(express.static('public'));
+
+const server=app.listen(process.env.PORT || 3000);
+const io = require('./socket').init(server);
+
+io.on('connection', socket => {
+    console.log(socket.id);
+    socket.on('chat' , data => {
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('hum' , data2 => {
+        io.sockets.emit('temp', "Veri geldiiii"+data2);
+    })
+})
---
+++ public/chat.js
@@ -0,0 +1,24 @@
+const socket= io.connect('https://makuse-socket.herokuapp.com');
+
+
+const sender= document.getElementById('sender');
+const message= document.getElementById('message');
+const submitBtn= document.getElementById('submitBtn');
+const output= document.getElementById('output');
+const feedback= document.getElementById('feedback');
+
+
+submitBtn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        sender:sender.value
+    })
+})
+
+socket.on('chat', data => {
+    output.innerHTML +='<p><strong>' + data.sender+ '</strong>' + data.message + '</p>';
+})
+
+socket.on('temp', data2 => {
+    output.innerHTML +='<p>aaaa'+ data2 +'</p>';
+})
